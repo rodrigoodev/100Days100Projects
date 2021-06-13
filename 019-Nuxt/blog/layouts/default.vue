ddabd494f3a1d1bb87762bc7b2ec7f2335aea21a
RodrigoGabrielRB <rodrigorgrb@gmail.com>
2021-06-12 21:41:07 -0300

    Feat project 019-nuxt

---
+++ 019-Nuxt/blog/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+
+  font-family: sans-serif;
+}
+
+body{
+  background-color: #eef;
+}
+
+a{
+  text-decoration: none;
+}
+
+p{
+  margin-bottom: 15px;
+}
+
+p:last-of-type{
+  margin-bottom:0px;
+}
+</style>
