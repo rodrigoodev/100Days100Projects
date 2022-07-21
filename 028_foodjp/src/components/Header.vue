a983b7613a03b1e2a63732ad0b0e15e30653936b
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-07-20 23:50:16 -0300

    feat: code

--- 028_foodjp/src/components/Header.vue
+++ 028_foodjp/src/components/Header.vue
@@ -4,14 +4,20 @@
       <Logo />
     </div>
     <div class="header__menu">
-      <ul>
-        <li><a href="#">Inicio</a></li>
-        <li><a href="#">Cardápio</a></li>
-        <li><a href="#">Sobre</a></li>
+      <ul class="header__menu-list">
+        <li class="header__menu-item">
+          <a href="#" class="header__link">Inicio</a>
+        </li>
+        <li class="header__menu-item">
+          <a href="#" class="header__link">Cardápio</a>
+        </li>
+        <li class="header__menu-item">
+          <a href="#" class="header__link">Sobre</a>
+        </li>
       </ul>
     </div>
     <div>
-      <button>Entrar</button>
+      <button class="header__button">Entrar</button>
     </div>
   </header>
 </template>
@@ -26,4 +32,52 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px 0;
+  align-items: center;
+
+  &__menu-list {
+    display: flex;
+    gap: 91px;
+  }
+
+  &__link {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 29px;
+    color: #292929;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &__button {
+    width: 188px;
+    height: 49px;
+    border-radius: 10px;
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 29px;
+    color: #292929;
+    border: 2px solid #292929;
+    background-color: transparent;
+    transition: 0.5s;
+
+    &:hover {
+      background-color: #292929;
+      color: #fbfbfb;
+    }
+  }
+}
+</style>
