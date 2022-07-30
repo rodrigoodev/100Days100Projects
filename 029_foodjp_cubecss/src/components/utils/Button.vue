0665eaa7c579d245f62c25799cbe0c5567409bbf
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-07-30 18:56:41 -0300

    feat: add button

---
+++ 029_foodjp_cubecss/src/components/utils/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="btn btn-primary" @click="onClick">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  methods: {
+    onClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+
+<style></style>
