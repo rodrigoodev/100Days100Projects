70e526b7ce85cd38e87594aeca7d48f91d2bf91e
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-07-29 22:45:43 -0300

    feat: finalizando footer

---
+++ 028_foodjp/src/components/Maps.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="maps-wrap">
+    <h2>Localização</h2>
+    <div class="maps" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Maps",
+};
+</script>
+
+<style lang="scss">
+.maps-wrap {
+  width: 1250px;
+  margin: 100px auto;
+  h2 {
+    text-align: left;
+  }
+}
+
+.maps {
+  width: 1250px;
+  margin: 48px auto;
+  height: 400px;
+  background-color: red;
+}
+</style>
