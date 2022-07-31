308015a8afe81ebf036a8d6a080b1f8ee4eb4af1
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-07-31 12:52:59 -0300

    initial commit

---
+++ 031_media-browser/.eleventy.js
@@ -0,0 +1,18 @@
+module.exports = (config) => {
+  config.addPassthroughCopy("./src/css");
+  config.addPassthroughCopy("./src/_redirects");
+
+  config.addCollection("videos", (collection) =>
+    collection.getFilteredByGlob("./src/videos/*.md")
+  );
+
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
